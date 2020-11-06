072c8988816f42d44de2b84933ae7665f0ff84bf
d0n0t <69791371+d0n0t@users.noreply.github.com>
2020-11-06 23:06:16 +0530

    Update run.py

--- run.py
+++ run.py
@@ -6,6 +6,15 @@ import time
 
 
 notebook_url = 'https://colab.research.google.com/github/mengwangk/dl-projects/blob/master/04_03_auto_ml_1.ipynb'
+chrome_options = webdriver.ChromeOptions()
+    chrome_options.binary_location = Config.GOOGLE_CHROME_BIN
+    chrome_options.add_argument('--ignore-certificate-errors')
+    chrome_options.add_argument("--test-type")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument('--disable-gpu')
 driver = webdriver.Chrome(chrome_options=chrome_options)
 driver.get(notebook_url)
 
