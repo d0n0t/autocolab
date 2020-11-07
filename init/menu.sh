d820f8b1c83ab24e6a65b75d339b4118ed021a74
d0n0t <69791371+d0n0t@users.noreply.github.com>
2020-11-07 17:22:57 +0530

    Update menu.sh

--- init/menu.sh
+++ init/menu.sh
@@ -1,10 +1,38 @@
-echo -e "s h a k a a l"
-echo "-----------------------------------------"
-echo "              main menu                  "
-echo "-----------------------------------------"
-1menu="1. Auth"  ;
-2menu="2. Colab" ;
-3menu="3. exit" ;
-1pick () { python3 run.py ; }
-2pick () { python3 run.py ; }
-3pick () { exit 0 ; }
+
+
+function Log_in() {
+    echo ""
+	echo "Heading to kitchen !!"
+	python3 auth.py
+	echo ""
+}
+function Co_lab() {
+    echo ""
+	echo "Cooking Shakaal special XC"
+	python3 run.py
+	echo ""
+}
+menu(){
+green='\e[32m'
+blue='\e[34m'
+clear='\e[0m'
+ColorGreen(){
+	echo -ne $green$1$clear
+}
+ColorBlue(){
+	echo -ne $blue$1$clear
+}
+echo -ne "
+ShaKaaL Restaurants
+$(ColorGreen '1)') Login In Google
+$(ColorGreen '2)') Run Colaboratory
+$(ColorGreen '0)') Exit
+$(ColorBlue 'What do you want to order?:') "
+        read a
+        case $a in
+	        1) Log_in ; menu ;;
+	        2) Co_lab ; menu ;;
+			0) exit 0 ;;
+			*) echo -e $red"Wrong option."$clear; WrongCommand;;
+        esac
+}
