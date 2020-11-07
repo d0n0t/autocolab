043df6830dd81c6c62724f7096799cd04927e8d1
d0n0t <69791371+d0n0t@users.noreply.github.com>
2020-11-07 17:07:02 +0530

    Update menu.sh

--- init/menu.sh
+++ init/menu.sh
@@ -1,4 +1,16 @@
 echo -e "s h a k a a l"
 echo "-----------------------------------------"
-echo "           main menu                     "
+echo "              main menu                  "
 echo "-----------------------------------------"
+echo " "
+echo " "
+echo "1)auth to google"
+echo "2) run colab"
+echo "3) exit"
+echo -n "enter number:"
+echo " "
+read 1
+read 2
+read 3
+{$3} exit 0
+
