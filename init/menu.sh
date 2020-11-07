71e7472613ec8bc11929cd6fe10b6f1697525b5a
d0n0t <69791371+d0n0t@users.noreply.github.com>
2020-11-07 17:11:58 +0530

    Update menu.sh

--- init/menu.sh
+++ init/menu.sh
@@ -2,15 +2,9 @@ echo -e "s h a k a a l"
 echo "-----------------------------------------"
 echo "              main menu                  "
 echo "-----------------------------------------"
-echo " "
-echo " "
-echo "1)auth to google"
-echo "2) run colab"
-echo "3) exit"
-echo -n "enter number:"
-echo " "
-read 1
-read 2
-read 3
-{$3} exit 0
-
+1menu="1. Auth"  ;
+2menu="2. Colab" ;
+3menu="3. exit" ;
+1pick () { python3 run.py ; }
+2pick () { python3 run.py ; }
+3pick () { exit 0 ; }
